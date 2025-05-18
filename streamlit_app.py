360e58b8445328b5b0215625d82efa266dd19ab5
pranavmanglani <sssfacts20@gmail.com>
2025-05-18 11:48:31 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,18 @@ import streamlit as st
 
 # Function to update the variable.  Now also stores input.
 def update_variable():
-    st.session_state.my_variable += 1
+    #st.session_state.my_variable += 1
     #st.write(f"Variable updated to: {st.session_state.my_variable}")
-    st.session_state.my_input_value = input_text # Store the input
+    st.session_state.my_input_value = username
+    st.session_state.my_input_value = password# Store the input
 
 # Display the current value
-st.write(f"Current value of my_variable: {st.session_state.my_variable}")
+#st.write(f"Current value of my_variable: {st.session_state.my_variable}")
 
 # Create the button.
-st.button("Click me to increment the variable", on_click=update_variable)
+#st.button("Click me to increment the variable", on_click=update_variable)
+username = st.text_input("Username", value="")#No label for initial value.
 
-# Create a text input box
-input_text = st.text_input("Enter some text:", value="") #No label for initial value.
+password = st.text_input("Password",value="")
 
-# Display the stored input
-st.write(f"You entered: ",input_text) #Show the stored value
+
